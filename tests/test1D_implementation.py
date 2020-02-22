378c6644dcc76a46098c988fb2109b2cc0e95cf9
Felix Zapata <jose.zapatau@gmail.com>
2020-02-22 19:05:00 +0100

    Generated 1D Design of Griffon Engine.

--- tests/test1D_implementation.py
+++ tests/test1D_implementation.py
@@ -16,6 +16,7 @@ import os.path                                                # Import os.path
 import pprint                                                 # Import pprint
 import json                                                   # Import json
 import numpy as np                                            # Import numpy library
+from copy import deepcopy                                     # Import the deepcopy method
 
 
 # ----------------------- FUNCTION DEFINITIONS ----------------------- #
@@ -44,7 +45,7 @@ def test_1d_implementation_single_circular_port():
     # ------------ Define parameters:
 
     geometric_params = {'L': 4.572,
-                        'r_init':0.152/2,
+                        'r_init': 0.152/2,
                         'r_ext': 0.5,
                         'N': 12}
 
@@ -55,16 +56,14 @@ def test_1d_implementation_single_circular_port():
                          'times': [0.1, 20, 60]}
 
     # ------------- Generate objects:
-
+    geometric_params['regressionModel'] = Reg.TwoRegimesMarxmanAndFloodedModel(**combustion_table)
     geometry_obj = Geom.SingleCircularPort1D(**geometric_params)
     nozzle_obj = Noz.Nozzle(**nozzle_params)
-    regression_mod = Reg.SingleRegimeMarxmanModel(**combustion_table)
 
     # Instantiate the combustion module
     combustion_obj = Combustion.CombustionObject1D(json_interpreter=json_interpreter,
                                                    nozzle_object=nozzle_obj,
-                                                   geometry_object=geometry_obj,
-                                                   regression_model=regression_mod)
+                                                   geometry_object=geometry_obj)
 
     # -------------- Run simulation & Plot:
 
@@ -87,36 +86,51 @@ def test_1d_implementation_single_circular_port_balanced_nozzle():
 
     # ------------ Generate the data-layer:
 
-    json_interpreter = generate_data_layer("Griffon Data - ABS - H2O2 - 36 bar.json")
+    json_interpreter = generate_data_layer("Griffon Data - Mock.json")
     combustion_table = json_interpreter.return_combustion_table()
 
     # ------------ Define parameters:
 
     geometric_params = {'L': 0.4,
-                        'r_init':0.03,
+                        'r_init': 0.052/2,
+                        'r_final': 0.094/2,
+                        'r_exit': 0.08/2,
+                        'exit_depth': 0.02,
+                        'depth': 0.3,
                         'r_ext': 0.05,
                         'N': 200}
 
     nozzle_params = {'At': 0.000589, 'expansion': 5.7, 'lambda_e': 0.98, 'erosion': 0}
 
-    simulation_params = {'ox_flow': 1.43, 'safety_thickness': 0.005, 'dt': 0.01,
-                         'max_burn_time': None, 'tol_press': 1e-1}
+    simulation_params = {'ox_flow': 1.08, 'safety_thickness': 0.004, 'dt': 0.05,
+                         'max_burn_time': 4.6, 'tol_press': 1e-3}
 
     # ------------- Generate objects:
-
-    geometry_obj = Geom.SingleCircularPort1D(**geometric_params)
+    geometric_params['regressionModel'] = Reg.TwoRegimesMarxmanAndFloodedModel(**combustion_table)
+    geometry_obj = Geom.ConicCircularPort1D(**geometric_params)
     nozzle_obj = Noz.Nozzle(**nozzle_params)
-    regression_mod = Reg.MarxmanAndConstantFloodingRegimeModel(**combustion_table)
+
+    # -------------- Generate deep copy of geometry object:
+
+    geometry_object_original = deepcopy(geometry_obj)
 
     # Instantiate the combustion module
     combustion_obj = Combustion.CombustionObject1D(json_interpreter=json_interpreter,
                                                    nozzle_object=nozzle_obj,
-                                                   geometry_object=geometry_obj,
-                                                   regression_model=regression_mod)
+                                                   geometry_object=geometry_obj)
 
     # -------------- Run simulation & Plot:
 
     combustion_obj.run_balanced_nozzle_analysis(**simulation_params)
+
+    # -------------- Export the results to File:
+
+    geometry_obj.export_geometry(file_name="C:/Users/Felix Zapata/Google Drive/MSc in AE ISAE - SUPAERO/"
+                                                       "Griffon Project/Design Files/1D March Test Design/"
+                                                       "geometry_after_burn_1D.txt")
+    geometry_object_original.export_geometry(file_name="C:/Users/Felix Zapata/Google Drive/MSc in AE ISAE - SUPAERO/"
+                                                       "Griffon Project/Design Files/1D March Test Design/"
+                                                       "original_geometry_1D.txt")
 
     # Print the module
     print(combustion_obj)
@@ -127,9 +141,8 @@ def test_1d_implementation_single_circular_port_balanced_nozzle():
     # Show plots
     plt.show()
 
-
-
 # ------------------------- MAIN ------------------------ #
+
 
 if __name__ == '__main__':
 
